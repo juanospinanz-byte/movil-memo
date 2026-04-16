f20254f6db0df7e82b21d396038b7ac4102738fe
Juanes <juan.ospinanz@amigo.edu.co>
2026-04-16 18:50:45 -0500

    se pone la paleta de colores

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,15 +1,15 @@
-export default{
+export default {
     //Colores principales y variantes
-    principal: '#2d0af5',
-    variante1: '#1a0a8c',
-    variante2: '#4d3bff',
+    principal: '#003B5F',
+    variante1: '#1998CC',
+    variante2: '#146A96',
     variante3: '#6e5cff',
     variante4: '#8f7dff',
     variante5: '#b09eff',
 
     //Colores para el texto y las sombras
-    defecto: '#faf7f7',
-    subtitilo: '#c0c0c0',
+    defecto: '#08854D',
+    subtitilo: '#095859',
     suave: '#e0e0e0',
     delicado: '#f0f0f0',
     iluminado: '#ffffff',
@@ -17,7 +17,7 @@ export default{
 
     //Colores para los background o fondos
     fondloClaro: '#f5f5f5',
-    fondloOscuro: '#500808',
+    fondloOscuro: '#003B5F',
 
     //colores de alertas y acciones
     alerta: '#ff4d4d',
@@ -26,9 +26,9 @@ export default{
     informacion: '#4dfff3',
 
     //Gradientes
-    gradientePrimario: ['#007bff', '#00d4ff'],
-    gradienteSecundario: ['#ff7f50', '#ff6347'],
-    
+    gradientePrimario: ['#003B5F', '#1998CC'],
+    gradienteSecundario: ['#08854D', '#095859'],
+
 
 
 }
